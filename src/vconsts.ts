5dc36d254703290a2e7f47eedf240f2def6695fb
darthzeran <ryan.mort@studentbody.byu.edu>
2026-02-19 13:02:50 -0700

    Update and rename vconsts.js to vconsts.ts

--- src/vconsts.ts
+++ src/vconsts.ts
@@ -1,7 +1,10 @@
+import {BattleCard, CharacterMap, Location, SpecialCard} from './types';
+
 export const GOOD_INDEXES = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 export const BAD_INDEXES = [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
 
-export const LOCATIONS = {
+
+export const LOCATIONS: Record<string, Location> = {
     MORDOR: {
         title: 'MORDOR',
         description: 'MORDOR',
@@ -199,7 +202,7 @@ export const LOCATIONS = {
     },
 }
 
-export const GOOD_CARDS = [
+export const GOOD_CARDS: BattleCard[] = [
     {id: 0, value: 1, type: 'strength'},
     {id: 1, value: 2, type: 'strength'},
     {
@@ -236,7 +239,7 @@ export const GOOD_CARDS = [
     },
 ]
 
-export const EVIL_CARDS = [
+export const EVIL_CARDS: BattleCard[] = [
     {id: 0, value: 1, type: 'strength'},
     {id: 1, value: 2, type: 'strength'},
     {
@@ -268,7 +271,7 @@ export const EVIL_CARDS = [
     },
 ]
 
-export const GOOD_SPECIAL_CARDS = [
+export const GOOD_SPECIAL_CARDS: SpecialCard[] = [
     {
         id: 1,
         title: 'Shadowfax',
@@ -292,7 +295,7 @@ export const GOOD_SPECIAL_CARDS = [
     },
 ]
 
-export const EVIL_SPECIAL_CARDS = [
+export const EVIL_SPECIAL_CARDS: SpecialCard[] = [
     {
         id: 5,
         title: 'Palantir',
@@ -316,7 +319,7 @@ export const EVIL_SPECIAL_CARDS = [
     },
 ]
 
-export const GOOD_CHARS = {
+export const GOOD_CHARS:CharacterMap = {
     'FRODO': {
         name: 'Frodo',
         id: 'FRODO',
@@ -418,7 +421,7 @@ export const GOOD_CHARS = {
     },
 }
 
-export const EVIL_CHARS = {
+export const EVIL_CHARS: CharacterMap = {
     'URUKHAI': {
         name: 'The Uruk-Hai',
         id: 'URUKHAI',
