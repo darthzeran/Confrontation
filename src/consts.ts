f12283d080d78ea591c4e58bc84ecd7e63d94a0c
darthzeran <ryan.mort@studentbody.byu.edu>
2026-02-24 19:55:05 -0700

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -383,20 +383,9 @@ export const CLASSIC_GOOD_CHARS: CharacterMap = {
         description: "If Boromir is in a battle, both characters are  defeated immediately. The only exceptions  are the Warg (in which case Boromir’s ability is ignored for that battle) and when Boromir uses the  Tunnel of Moria while the Balrog occupies Caradhras (in  which case Boromir is defeated without a battle)."
     },
 }
-export const CLASSIC_GOOD_NAMES = {
-    CLASSICFRODO: 'CLASSICFRODO',
-    CLASSICSAM: 'CLASSICSAM',
-    PIPPIN: 'PIPPIN',
-    MERRY: 'MERRY',
-    CLASSICGANDALF: 'CLASSICGANDALF',
-    CLASSICARAGORN: 'CLASSICARAGORN',
-    LEGOLAS: 'LEGOLAS',
-    GIMLI: 'GIMLI',
-    BOROMIR: 'BOROMIR',
-}
 export const CLASSIC_EVIL_CHARS: CharacterMap = {
     'BALROG': {
-        name: 'The Balrog',
+        name: 'Balrog',
         id: 'BALROG',
         value: 5,
         classic: true,
@@ -445,7 +434,7 @@ export const CLASSIC_EVIL_CHARS: CharacterMap = {
         description: "When the Orcs attack, they immediately  defeat the first Fellowship character attacked  in the region. Gimli immediately defeats the  Orcs and is therefore unharmed in any battle against  them. If the Orcs attack Boromir, both characters are  defeated. If there are additional Fellowship characters in  the region, the Orc’s special ability is ignored for those  subsequent battles. The Orcs have no special ability if  attacked by a Fellowship character.  If the Orcs attack Frodo first, Frodo may retreat sideways before the Orcs can defeat him. In this particular  case, this is still considered the first attack by the Orcs,  and they will have no special ability for the remainder of  the turn."
     },
     'WARG': {
-        name: 'The Warg',
+        name: 'Warg',
         id: 'WARG',
         value: 2,
         classic: true,
@@ -458,17 +447,6 @@ export const CLASSIC_EVIL_CHARS: CharacterMap = {
         classic: true,
         description: "When it comes to playing cards in a battle  with the Cave Troll, the Sauron player’s card  is ignored. The Sauron player must still play  and discard a card, even though that card has no effect in  the battle."
     },
-}
-export const CLASSIC_EVIL_NAMES = {
-    BALROG: "BALROG",
-    SHELOB: "SHELOB",
-    CLASSICWITCHKING: "CLASSICWITCHKING",
-    CLASSICFLYINGNAZGUL: "CLASSICFLYINGNAZGUL",
-    BLACKRIDER: "BLACKRIDER",
-    CLASSICSARUMAN: "CLASSICSARUMAN",
-    CLASSICORCS: "CLASSICORCS",
-    WARG: "WARG",
-    CAVETROLL: "CAVETROLL",
 }
 export const VARIANT_GOOD_CHARS: CharacterMap = {
     'VARIANTFRODO': {
@@ -579,17 +557,6 @@ export const VARIANT_GOOD_CHARS: CharacterMap = {
             "new Fellowship character’s text as if he had been\n" +
             "attacked."
     },
-}
-export const VARIANT_GOOD_NAMES = {
-    VARIANTFRODO: 'VARIANTFRODO',
-    VARIANTSAM: 'VARIANTSAM',
-    ELROND: 'ELROND',
-    VARIANTGANDALF: 'VARIANTGANDALF',
-    VARIANTARAGORN: 'VARIANTARAGORN',
-    TREEBEARD: 'TREEBEARD',
-    FARAMIR: 'FARAMIR',
-    THEODEN: 'THEODEN',
-    SMEAGOL: 'SMEAGOL',
 }
 export const VARIANT_EVIL_CHARS: CharacterMap = {
     'URUKHAI': {
@@ -703,7 +670,41 @@ export const VARIANT_EVIL_CHARS: CharacterMap = {
             "character has already retreated."
     },
 }
-export const VARIANT_EVIL_NAMES = {
+
+const CLASSIC_GOOD_NAMES = {
+    CLASSICFRODO: 'CLASSICFRODO',
+    CLASSICSAM: 'CLASSICSAM',
+    PIPPIN: 'PIPPIN',
+    MERRY: 'MERRY',
+    CLASSICGANDALF: 'CLASSICGANDALF',
+    CLASSICARAGORN: 'CLASSICARAGORN',
+    LEGOLAS: 'LEGOLAS',
+    GIMLI: 'GIMLI',
+    BOROMIR: 'BOROMIR',
+}
+const CLASSIC_EVIL_NAMES = {
+    BALROG: "BALROG",
+    SHELOB: "SHELOB",
+    CLASSICWITCHKING: "CLASSICWITCHKING",
+    CLASSICFLYINGNAZGUL: "CLASSICFLYINGNAZGUL",
+    BLACKRIDER: "BLACKRIDER",
+    CLASSICSARUMAN: "CLASSICSARUMAN",
+    CLASSICORCS: "CLASSICORCS",
+    WARG: "WARG",
+    CAVETROLL: "CAVETROLL",
+}
+const VARIANT_GOOD_NAMES = {
+    VARIANTFRODO: 'VARIANTFRODO',
+    VARIANTSAM: 'VARIANTSAM',
+    ELROND: 'ELROND',
+    VARIANTGANDALF: 'VARIANTGANDALF',
+    VARIANTARAGORN: 'VARIANTARAGORN',
+    TREEBEARD: 'TREEBEARD',
+    FARAMIR: 'FARAMIR',
+    THEODEN: 'THEODEN',
+    SMEAGOL: 'SMEAGOL',
+}
+const VARIANT_EVIL_NAMES = {
     URUKHAI: "URUKHAI",
     VARIANTWITCHKING: "VARIANTWITCHKING",
     VARIANTSARUMAN: "VARIANTSARUMAN",
