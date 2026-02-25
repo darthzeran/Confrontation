279146406562a9f68fafb8001014c807585b12d9
darthzeran <ryan.mort@studentbody.byu.edu>
2026-02-24 20:09:46 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,9 +27,6 @@ const App = () => {
     const [matchId, setMatchId] = useState('')
     const [playerId, setPlayerId] = useState('')
 
-    // const classicGame = matchId?.[0] === 'a'
-    // const variantGame = matchId?.[0] === 'b'
-
     return (
         <div>
             <ToastContainer
